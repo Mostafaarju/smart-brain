1b47415062f3b5fef568e6de06a08a6cecae1018
Raju Talukder <63764709+Mostafaarju@users.noreply.github.com>
2022-03-14 19:45:50 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Logo from './components/Logo/Logo';
+import Navigation from './components/Navigation/Navigation';
+import Rank from './components/Rank/Rank';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      input: '',
+    };
+  }
+
+  onInputChange = event => {
+    console.log(event);
+  };
+
+  render() {
+    return (
+      <div className='App'>
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm />
+      </div>
+    );
+  }
 }
 
 export default App;
